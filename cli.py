9fb4774e750e3f07d2b9697b2f0d4ad7752f47e0
Carl Vondrick <cvondric@ics.uci.edu>
2011-01-16 15:13:21 -0800

    cleanup

--- cli.py
+++ cli.py
@@ -3,14 +3,10 @@ from turkic.cli import handler
 import turkic.database
 import models
 
-@handler("Add videos to be labeled.", "import")
-def importstuff(args):
+@handler("Add videos to be labeled.")
+def load(args):
     pass
 
 @handler("Output all job data.")
 def dump(args):
     pass
-
-@handler("Reinstalls the database.")
-def dbinit(args):
-    turkic.database.reinstall() 
