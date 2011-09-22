6e00549ff0a44d266906a373f284ed5e6df2d3fa
Carl Vondrick <cvondric@ics.uci.edu>
2011-09-22 12:24:42 -0700

    updated readme

--- vatic-install.sh
+++ vatic-install.sh
@@ -1,4 +1,4 @@
-sudo apt-get install -y git python-setuptools python-dev libavcodec-dev libavformat-dev libswscale-dev libjpeg62 libjpeg62-dev libfreetype6 libfreetype6-dev apache2 libapache2-mod-wsgi mysql-server-5.1 mysql-client-5.1 libmysqlclient-dev gfortran g77
+sudo apt-get install -y git python-setuptools python-dev libavcodec-dev libavformat-dev libswscale-dev libjpeg62 libjpeg62-dev libfreetype6 libfreetype6-dev apache2 libapache2-mod-wsgi mysql-server-5.1 mysql-client-5.1 libmysqlclient-dev gfortran 
 
 sudo easy_install -U SQLAlchemy wsgilog pil cython mysql-python munkres parsedatetime argparse
 sudo easy_install -U numpy
